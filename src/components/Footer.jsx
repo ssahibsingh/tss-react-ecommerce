8a7f0258aa6b6f41333fe604023912dfea59b530
ssahibsingh <snone181@gmail.com>
2023-06-20 22:07:40 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="mb-0 text-center">
-        <div className="d-flex align-items-center justify-content-center pb-5">
+      <footer className="mb-0 text-center bg-light">
+        <div className="d-flex align-items-center justify-content-center py-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.vercel.app" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
+            <p className="mb-3 mb-md-0">
+              Copyright &copy; TSS 2023
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
-              <i className="fa fa-github"></i>
-            </a>
           </div>
         </div>
       </footer>
